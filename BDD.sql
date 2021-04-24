7d4f3288f824664a8fa8388abea1390a427d9be6
Zelkins10 <mdoyen4@gmail.com>
2021-04-24 16:56:14 +0200

    Mise en auto-incrément des id à nouveau

--- BDD.sql
+++ BDD.sql
@@ -8,7 +8,7 @@
 #------------------------------------------------------------
 
 CREATE TABLE IMAC_Utilisateur(
-        id            Int NOT NULL ,
+        id            Int  Auto_increment  NOT NULL ,
         pseudo        Varchar (50) NOT NULL ,
         motDePasse    Varchar (50) NOT NULL ,
         prenom        Varchar (50) ,
@@ -26,7 +26,7 @@ CREATE TABLE IMAC_Utilisateur(
 #------------------------------------------------------------
 
 CREATE TABLE IMAC_Publication(
-        id                  Int NOT NULL ,
+        id                  Int  Auto_increment  NOT NULL ,
         texte               Longtext ,
         image               Varchar (50) ,
         date                Date NOT NULL ,
@@ -42,7 +42,7 @@ CREATE TABLE IMAC_Publication(
 #------------------------------------------------------------
 
 CREATE TABLE IMAC_Commentaire(
-        id                  Int NOT NULL ,
+        id                  Int  Auto_increment  NOT NULL ,
         contenu             Longtext NOT NULL ,
         date                Date NOT NULL ,
         id_IMAC_Utilisateur Int NOT NULL ,
@@ -59,7 +59,7 @@ CREATE TABLE IMAC_Commentaire(
 #------------------------------------------------------------
 
 CREATE TABLE IMAC_MessagePrive(
-        id                           Int NOT NULL ,
+        id                           Int  Auto_increment  NOT NULL ,
         image                        Varchar (50) ,
         date                         Date NOT NULL ,
         texte                        Longtext ,
