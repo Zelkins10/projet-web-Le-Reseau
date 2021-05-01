aa523dbb7c013944b59eaa42835ceebad07a5a6c
TaliaKah <anaisgougne@gmail.com>
2021-05-01 19:50:17 +0200

    maj

--- index.php
+++ index.php
@@ -14,36 +14,45 @@
 				echo "<a href='formPublication.php'>Nouvelle Publication</a><br>";
 				echo "<a href='messagesPrives.php'>Nouveau Message privé</a><br>";
 				echo "<a href='profil.php?pseudo=".$pseudo."'>Mon Compte</a><br>";
-				echo "<a href='logout.php'>Se déconnecter</a>";
-				$reponse = $bdd->query('SELECT * FROM IMAC_Publication JOIN
-					 						IMAC_AimerPublication ON id.IMAC_AimerPublication=id.IMAC_Publication JOIN
-											IMAC_Utilisateur ON id_IMAC_Utilisateur.IMAC_AimerPublication=id.IMAC_Utilisateur
-											WHERE MAX(COUNT(
-												SELECT * FROM IMAC_Utilisateur JOIN 
-												IMAC_AimerPublication ON id.IMAC_Utilisateur=id.IMAC_AimerPublication) 
-												WHERE pseudo="'.$pseudo.'"');
+				echo "<a href='logout.php'>Se déconnecter</a><br>";
+				$reponse = $bdd->query('SELECT pseudo,photoProfil,texte,image,date,COUNT(IMAC_AimerPublication.id_IMAC_Utilisateur) AS nombrelike
+										FROM IMAC_Publication JOIN 
+										IMAC_AimerPublication ON IMAC_AimerPublication.id=IMAC_Publication.id JOIN
+										IMAC_Utilisateur ON IMAC_AimerPublication.id_IMAC_Utilisateur=IMAC_Utilisateur.id 
+										WHERE pseudo="'.$pseudo.'" 
+										ORDER BY nombrelike');
 				
 			}
 			else{
 				echo "<a href='formInscription.php'>S'inscrire</a><br>";
-				echo "<a href='formConnexion.php'>Se connecter</a>";
-				$reponse = $bdd->query('SELECT * FROM IMAC_Publication JOIN
-										IMAC_AimerPublication ON id.IMAC_AimerPublication=id.IMAC_Publication JOIN
-			   							IMAC_Utilisateur ON id_IMAC_Utilisateur.IMAC_AimerPublication=id.IMAC_Utilisateur
-			   							WHERE MAX(COUNT(
-				   							SELECT * FROM IMAC_Utilisateur JOIN 
-								 			IMAC_AimerPublication ON id.IMAC_Utilisateur=id.IMAC_AimerPublication) 
-				   							WHERE pseudo="'.$pseudo.'"');
+				echo "<a href='formConnexion.php'>Se connecter</a><br>";
+				$reponse = $bdd->query('SELECT pseudo,photoProfil,texte,image,date,COUNT(IMAC_AimerPublication.id_IMAC_Utilisateur) AS nombrelike
+										FROM IMAC_Publication JOIN 
+										IMAC_AimerPublication ON IMAC_AimerPublication.id=IMAC_Publication.id JOIN
+										IMAC_Utilisateur ON IMAC_AimerPublication.id_IMAC_Utilisateur=IMAC_Utilisateur.id 
+										ORDER BY nombrelike');
+			
+
 			}
 			$compteur=0;
-			while($donnees=$response->fetch() OR $compteur>4){
-
-				echo $donnees['photoProfil']; 
-				echo $donnees['pseudo'];
-				echo $donnees['texte'];
-				echo $donnees['image'];
+			while ($donnees=$response->fetch() OR $compteur>4){
+				?>
+                <div class="auteurPublication">
+                    <?php echo $donnees['pseudo']; ?> </div>
+                <br>
+                <div class="photoProfilAuteur">
+                    <img src="<?php echo $donnees['photoProfil']; ?>" id="photo_profil"> </div> 
+                <div class="caracteristiques">Publié le 
+                    <?php echo $donnees['date']; ?> </div>
+                <br>
+                <div class="caracteristiques">
+                <img src="<?php echo $donnees['image']; ?>" id="img_publication"></div> 
+                <div class="caracteristiques"><?php echo $donnees['texte']; ?></div>
+                <br>
+				<?php
 				$compteur++;
 			}
+			$reponse->closeCursor();
 
 		?>
 	</body>
